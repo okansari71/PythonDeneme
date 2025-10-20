b9fe92e0abe8a090dc7771fbec752ae170df1d99
Okan Sari <Okansa@gmail.com>
2025-10-20 18:38:58 +0300

    Update ok

--- iterators.py
+++ iterators.py
@@ -1,19 +1,57 @@
-liste = [1,2,3,4,5]
+# liste = [1,2,3,4,5]
 
-iterator = iter(liste)
+# iterator = iter(liste)
 
 # print(next(iterator))
 # print(next(iterator))
 # print(next(iterator))
 
-for i in liste:
-    print (i)
+# for i in liste:
+#     print (i)
 
-iterator = iter(liste)
+# iterator = iter(liste)
+
+# liste = [1,2,3,4,5]
+
+# iterator = iter(liste)
+
+# while True:
+#     try:
+#         element = next(iterator)
+#         print(element)
+#     except StopIteration:
+#         break
+
+class MyNumbers:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.start <= self.stop:
+            x= self.start
+            self.start +=1
+            return x
+        else:
+            raise StopIteration
+
+list = MyNumbers(20,50)
+
+
+myiter = iter(list)
 
 while True:
     try:
-        element = next(iterator)
+        element = next(myiter)
         print(element)
     except StopIteration:
         break
+    
+# for x in list:
+#     print(x)
+
+
+
